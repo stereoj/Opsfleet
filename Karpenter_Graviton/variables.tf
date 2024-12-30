9f7739af8ea06d2b8140dbc818dfcbbba9f66821
StereoJ <stereoj@gmail.com>
2024-12-31 01:20:45 +0200

    Update variables.tf

--- Karpenter_Graviton/variables.tf
+++ Karpenter_Graviton/variables.tf
@@ -1,9 +1,4 @@
-variable "vpc_id" {
-  description = "The VPC ID where the EKS cluster will be deployed."
+variable "region" {
+  description = "Region to deploy the resources"
   type        = string
-}
-
-variable "subnet_ids" {
-  description = "List of subnet IDs in the VPC for the EKS cluster."
-  type        = list(string)
 }
