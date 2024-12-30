1541c61d3388a20664162df1c0cfeec5746598d1
StereoJ <stereoj@gmail.com>
2024-12-31 01:18:59 +0200

    Update karpenter.tf

--- Karpenter_Graviton/karpenter.tf
+++ Karpenter_Graviton/karpenter.tf
@@ -1,28 +1,77 @@
-# Add Helm provider to install Karpenter
-resource "helm_release" "karpenter" {
-  name       = "karpenter"
-  repository = "https://charts.karpenter.sh"
-  chart      = "karpenter"
-  version    = "0.36.2"
+# Karpenter default EC2NodeClass and NodePool
 
-  set {
-    name  = "controller.clusterName"
-    value = module.eks.cluster_name
-  }
-  set {
-    name  = "controller.clusterEndpoint"
-    value = module.eks.cluster_endpoint
-  }
-  set {
-    name  = "controller.serviceAccount.create"
-    value = "false"
-  }
-  set {
-    name  = "controller.serviceAccount.name"
-    value = "karpenter-service-account"
-  }
-  set {
-    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = "arn:aws:iam::${var.aws_account_id}:role/KarpenterControllerRole"
-  }
+resource "kubectl_manifest" "karpenter_default_ec2_node_class" {
+  yaml_body = <<YAML
+apiVersion: karpenter.k8s.aws/v1
+kind: EC2NodeClass
+metadata:
+  name: default
+spec:
+  role: "${local.node_iam_role_name}"
+  amiSelectorTerms: 
+  - alias: al2@latest
+  securityGroupSelectorTerms:
+  - tags:
+      karpenter.sh/discovery: ${local.name}
+  subnetSelectorTerms:
+  - tags:
+      karpenter.sh/discovery: ${local.name}
+  tags:
+    IntentLabel: apps
+    KarpenterNodePoolName: default
+    NodeType: default
+    intent: apps
+    karpenter.sh/discovery: ${local.name}
+    project: karpenter-blueprints
+YAML
+  depends_on = [
+    module.eks.cluster,
+    module.eks_blueprints_addons.karpenter,
+  ]
+}
+
+resource "kubectl_manifest" "karpenter_default_node_pool" {
+  yaml_body = <<YAML
+apiVersion: karpenter.sh/v1
+kind: NodePool
+metadata:
+  name: default 
+spec:  
+  template:
+    metadata:
+      labels:
+        intent: apps
+    spec:
+      requirements:
+        - key: kubernetes.io/arch
+          operator: In
+          values: ["amd64", "arm64"]
+        - key: "karpenter.k8s.aws/instance-cpu"
+          operator: In
+          values: ["4", "8", "16", "32", "48", "64"]
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["spot", "on-demand"]
+        - key: karpenter.k8s.aws/instance-category
+          operator: In
+          values: ["c", "m", "r", "i", "d"]
+      nodeClassRef:
+        name: default
+        group: karpenter.k8s.aws
+        kind: EC2NodeClass
+      kubelet:
+        containerRuntime: containerd
+        systemReserved:
+          cpu: 100m
+          memory: 100Mi
+  disruption:
+    consolidationPolicy: WhenEmptyOrUnderutilized
+    consolidateAfter: 1m
+    
+YAML
+  depends_on = [
+    module.eks.cluster,
+    module.eks_blueprints_addons.karpenter,
+    kubectl_manifest.karpenter_default_ec2_node_class,
+  ]
 }
